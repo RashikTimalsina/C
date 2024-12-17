c790637aa316c723b4b9e4950f951b936d01baa5
RashikTimalsina <rashiktimalsina21@gmail.com>
2024-12-17 18:01:38 +0545

    commit update

--- CPrograms/getchar.c
+++ CPrograms/getchar.c
@@ -3,30 +3,19 @@
 
 int main(){
 
+    char ch;
+    int n;
+    printf("\n Plese enter the character : ");
+    scanf(" %c", &ch); // Remove space after %c
 
-        char ch;
-        int n;
-        printf("\n Plese enter the character : ");
+    printf("\n Please enter the character numbers to be stored :");
+    scanf("%d", &n); // Remove space after %d
 
-        scanf("%c ",&ch);
+    int i = 0; // Initialize i to 0
+    while(i < n){ // Change condition to i < n
+        printf("The string formed is : %c\n", ch); // Add newline for better readability
+        i++; // Move increment inside the loop
+    }
 
-        printf("\n Please enter the character numbers to be stored ;");
-        scanf("%d ",&n);
-
-        ch=getchar();
-
-            int i=n;
-            while(i<=n){
-
-                printf("The string formed is : %c",ch);
-
-
-            }
-            i++;
-
-
-return 0;
-
-    
-
+    return 0;
 }
